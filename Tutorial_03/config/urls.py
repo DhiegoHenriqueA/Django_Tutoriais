9ef9ebb90aa2a16122873e6756b45315c316260d
dhiego <dhiego.azevedo17@gmail.com>
2022-08-18 11:02:22 -0300

    tutorial 2

--- Tutorial_03/config/urls.py
+++ Tutorial_03/config/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import HomePageView, AboutPageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('about/', AboutPageView.as_view(), name='about'),
 ]
